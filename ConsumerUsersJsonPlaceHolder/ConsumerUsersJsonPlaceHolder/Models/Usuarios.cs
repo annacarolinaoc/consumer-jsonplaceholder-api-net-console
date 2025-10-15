6a6949d2b644168ea915406b256cd3614bc59016
annacarolinaoc <annacarolina_oc@icloud.com>
2025-10-15 08:58:39 -0300

    Add files via upload

---
+++ ConsumerUsersJsonPlaceHolder/ConsumerUsersJsonPlaceHolder/Models/Usuarios.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerUsersJsonPlaceHolder.Models
+{
+    public class Usuarios
+    {
+        
+        public string nome { get; set; }
+        public string cidade { get; set; }
+        public string empresa { get; set; }
+    }
+}
---
+++ ConsumerUsersJsonPlaceHolder/ConsumerUsersJsonPlaceHolder/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Text.Json;
+using ConsumerUsersJsonPlaceHolder.Models;
+
+var enderecoUrl = "https://jsonplaceholder.typicode.com/users";
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage? respostaAPi = await client.GetAsync(enderecoUrl);
+
+    respostaAPi.EnsureSuccessStatusCode();
+
+    string respostaAPiJson = await respostaAPi.Content.ReadAsStringAsync();
+
+    Usuarios? users = JsonSerializer.Deserialize<Usuarios>(respostaAPiJson);
+
+    Console.WriteLine("Nome: " + users.nome);
+    Console.WriteLine("Cidadde: " + users.cidade);
+    Console.WriteLine("Empresa: " + users.empresa);
+}
+catch (System.Exception e)
+{
+    
+    Console.WriteLine("Aconteceu um erro:\n" + e.Message);
+}
